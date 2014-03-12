e70dfaa910bb2839a3e6c2c790cdb9f7b5828c92
Aleksandar Tokarev <tokarevaleksandar@yahoo.com>
2014-03-12 22:11:14 +0100

    Added password and encrypted_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+	attr_accessor :password
+	#attr_accessible :name, :email, :password, :password_confirmation
 	#attr_accessible :name, :email  // ova e loso iam problemi nekoi
 	validates_presence_of :name, :email
 	validates :name, :length => { :maximum => 50}
@@ -7,5 +9,7 @@ class User < ActiveRecord::Base
     # /i na kraj e za case insensitive, a \A na pocetok e pocetok na string, a \z na kraj
     # e kraj na string
    # validates :name, presence: true
+   validates :password, :presence => true, :confirmation => true
+   validates :password, :length => { :within => 6..40}
     
 end
